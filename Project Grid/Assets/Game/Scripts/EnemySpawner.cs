c6e919ab8e0d584cf61bee0799d12639a1538c2d
bartanoren <bartanoren@hotmail.com>
2022-10-15 15:45:49 +0200

    Script for spawner
    enemies are only spawned after room enter collision (can be improved later on)

--- Project Grid/Assets/Game/Scripts/EnemySpawner.cs
+++ Project Grid/Assets/Game/Scripts/EnemySpawner.cs
@@ -16,22 +16,48 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField]
     private float spawnDistance = 10f;
+    
+    [SerializeField]
+    private double maxEnemyCount = 0d;
 
+    Collider2D m_Collider;
+
+    private bool spawnerEnabled = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        print("running");
+        spawnVec = transform.position;
+
+        m_Collider = GetComponent<Collider2D>();
         
-       spawnVec = transform.position ;
-
-        StartCoroutine(spawnEnemy(spawnerInterval, spawnerPrefab));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, spawnVec + new Vector3(Random.Range(-1 * spawnDistance, spawnDistance), Random.Range(-1 * spawnDistance, spawnDistance), 0) , Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        print("OnTriggerEnter2D detected");
+        if (!spawnerEnabled)
+        {
+            spawnerEnabled = true;
+            StartCoroutine(spawnEnemy(spawnerInterval, spawnerPrefab, maxEnemyCount));
+        }
     }
 
+
+    //TODO: ad ontrigger exit to destroy all enemies in this room and also destroy the spawner
+
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy, double count)
+    {
+        if (count > 0){
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, spawnVec + new Vector3(Random.Range(-1 * spawnDistance, spawnDistance), 
+        Random.Range(-1 * spawnDistance, spawnDistance), 0) , Quaternion.identity);
+        count--;
+        StartCoroutine(spawnEnemy(interval, enemy, count));
+        }
+        
+    }
 }
